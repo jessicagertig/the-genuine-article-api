788bffe3c076b069aad27b9b887f8de787cae347
Jessica Cooper <jessicagcooper@gmail.com>
2021-02-01 23:22:16 -0600

    feat(images model & router): began to write images model and router
    Created table for main image. Began functions for image uploading in images model and images router.
    Added router to server.js.

---
+++ api/items-images/items-images-router.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const Images = require('./items-images-model');
+const { ImageUploader } = require('../utils/imageUploader');
+const {
+	parseFormData,
+	readUploadedFile
+} = require('../utils/parseFiles');
+
+router.post('/main-image/:item_id', async (req, res) => {
+	const item_id = req.params.item_id;
+	const parsedData = await parseFormData(req);
+	const body = await readUploadedFile(parsedData, 'image');
+	const uploader = new ImageUploader('main_image');
+	const main_image_url = await uploader.upload(
+		item_id,
+		parsedData.files['image'].name,
+		body
+	);
+	console.log('main_image_url', main_image_url);
+	console.log('item_id', item_id);
+	Images.addMainImage(main_image_url, item_id)
+		.then((img) => {
+			res.status(201).json(img);
+		})
+		.catch((error) => {
+			res
+				.status(500)
+				.json({ message: 'Error on server end', error });
+		});
+});
+
+module.exports = router;
