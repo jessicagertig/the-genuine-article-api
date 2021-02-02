e33923341db874d4ab6b3200f0615229b2acd347
Jessica Cooper <jessicagcooper@gmail.com>
2021-02-01 23:15:48 -0600

    refactor(parsefiles): remove ternary, add param to new Incoming Form, move module.exports to end

--- api/utils/parseFiles.js
+++ api/utils/parseFiles.js
@@ -1,15 +1,9 @@
 const { IncomingForm } = require('formidable');
 const promises = require('fs/promises');
 
-module.exports = {
-	parseFormData,
-	readUploadedFile
-};
-
 const parseFormData = async (req) => {
 	return await new Promise((resolve, reject) => {
-		const form = new IncomingForm();
-
+		const form = new IncomingForm({ keepExtensions: true });
 		form.parse(req, (err, fields, files) => {
 			if (err) return reject(err);
 			resolve({ fields, files });
@@ -18,9 +12,14 @@ const parseFormData = async (req) => {
 };
 
 const readUploadedFile = async (data, key) => {
-	const path = data.files[key]?.path || '';
+	const path = data.files[key].path;
 	if (path) {
 		return await promises.readFile(path);
 	}
 	return null;
+};
+
+module.exports = {
+	parseFormData,
+	readUploadedFile
 };
