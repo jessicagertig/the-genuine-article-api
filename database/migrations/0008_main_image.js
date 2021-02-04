baa697b95b9a7d6efe3455139c8189c16526e377
Jessica <49106594+JessicaGCooper@users.noreply.github.com>
2021-02-04 03:49:38 -0600

    Feature/s3 uploads multiple sizes (#3)
    * feat(complete main img uploader): completed ImageUploader and MainImageUploader
    
    Changed ImageUploader to return array of urls for all sizes of uploaded image including original.
    Altered names and sizes returned by MainImageUploader.
    
    * feat(addmainimage): addMainImage now adds urls for all sizes of uploaded image to main_image table
    
    * feat(uploads multiple sizes): main-image now uploads multiple sizes and post urls to db

--- database/migrations/0008_main_image.js
+++ database/migrations/0008_main_image.js
@@ -2,6 +2,11 @@ exports.up = async function (knex) {
 	await knex.schema.createTable('main_image', function (tbl) {
 		tbl.increments().primary();
 		tbl.string('main_image_url').notNullable();
+		tbl.string('main_large_url').notNullable();
+		tbl.string('main_display_url').notNullable();
+		tbl.string('main_admin_upload_url').notNullable();
+		tbl.string('main_small_url').notNullable();
+		tbl.string('main_thumb_url').notNullable();
 		tbl
 			.integer('item_id')
 			.references('id')
@@ -12,5 +17,5 @@ exports.up = async function (knex) {
 };
 
 exports.down = async function (knex) {
-	await knex.schema.dropTableIfExists('colors');
+	await knex.schema.dropTableIfExists('main_image');
 };
