30849d89f9348b6c5c7c1f3fdaf9eb7d261e14e8
Jessica Cooper <jessicagcooper@gmail.com>
2021-02-21 02:33:06 -0600

    Added function to insert main img sizes urls to table.

--- api/items-images/items-images-model.js
+++ api/items-images/items-images-model.js
@@ -1,30 +1,33 @@
 const db = require('../../database/db-config');
 
 //findItemById
-function findMainImagesByItemId(id) {
+function findMainImageByItemId(id) {
 	return db('main_image').where({ id }).first();
 }
-
-// function addMainImage(main_image, item_id) {
-// 	const fieldsToInsert = {
-// 		main_image_url: main_image[0],
-// 		main_large_url: main_image[1],
-// 		main_display_url: main_image[2],
-// 		main_admin_upload_url: main_image[3],
-// 		main_small_url: main_image[4],
-// 		main_thumb_url: main_image[5],
-// 		item_id: item_id
-// 	};
 
 async function addMainImage(main_image_info) {
 	const [id] = await db('main_image').insert(main_image_info, 'id');
 
-	return findMainImagesByItemId(id);
+	return findMainImageByItemId(id);
 }
 
-//todo check for entry with that item_id and overwrite it if exists instead of creating new row
+async function addMainImageSizes(baseUrl, fileName, item_id) {
+	const fieldsToInsert = {
+		large: `${baseUrl}/large_${fileName}`,
+		display: `${baseUrl}/display_${fileName}`,
+		admin_upload: `${baseUrl}/admin_upload_${fileName}`,
+		small: `${baseUrl}/small_${fileName}`,
+		thumb: `${baseUrl}/thumb_${fileName}`,
+		item_id: item_id
+	};
+	//todo check for entry with that item_id and overwrite it if exists instead of creating new row
+	return await db('main_image_sizes')
+		.insert(fieldsToInsert)
+		.returning('*');
+}
 
 module.exports = {
 	addMainImage,
-	findMainImagesByItemId
+	addMainImageSizes,
+	findMainImageByItemId
 };
