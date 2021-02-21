a14b8f999fbe7b5286cf04ff074660835c38f289
Jessica Cooper <jessicagcooper@gmail.com>
2021-02-21 02:34:28 -0600

    Added route for posting resized main image urls after upload to S3.

--- api/items-images/items-images-router.js
+++ api/items-images/items-images-router.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
 const Images = require('./items-images-model');
-const { ImageUploader } = require('../utils/imageUploader');
+const {
+	ImageUploader,
+	ResizedMainImageUploader
+} = require('../utils/imageUploader');
 const {
 	parseFormData,
 	readUploadedFile
 } = require('../utils/parseFiles');
 
-router.put('/main-image/:item_id', async (req, res) => {
+router.post('/main_image/:item_id', async (req, res) => {
 	const item_id = req.params.item_id;
 	const parsedData = await parseFormData(req);
 	const body = await readUploadedFile(parsedData, 'image');
@@ -15,8 +18,8 @@ router.put('/main-image/:item_id', async (req, res) => {
 		parsedData.files['image'].hash,
 		'hex'
 	).toString('base64');
-	const uploadOriginalImg = new ImageUploader('original_image');
-	const main_image_url = await uploadOriginalImg.uploadOriginalImage(
+	const upload = new ImageUploader('original_image');
+	const main_image_url = await upload.uploadOriginalImage(
 		item_id,
 		parsedData.files['image'].name, //fileName
 		body,
@@ -27,6 +30,31 @@ router.put('/main-image/:item_id', async (req, res) => {
 	Images.addMainImage({ main_image_url, item_id })
 		.then((img) => {
 			res.status(201).json({ ...img, main_image_url });
+		})
+		.catch((error) => {
+			res
+				.status(500)
+				.json({ message: 'Error on server end', error });
+		});
+});
+
+router.post('/main_image_sizes/:item_id', async (req, res) => {
+	const item_id = req.params.item_id;
+	const parsedData = await parseFormData(req);
+	const body = await readUploadedFile(parsedData, 'image');
+	const contentType = parsedData.files['image'].type;
+	const fileName = parsedData.files['image'].name;
+	const upload = new ResizedMainImageUploader('main_image_sizes');
+	const baseUrl = await upload.uploadResizedImages(
+		item_id,
+		fileName,
+		body,
+		contentType
+	);
+
+	Images.addMainImageSizes(baseUrl, fileName, item_id)
+		.then((img) => {
+			res.status(201).json({ ...img });
 		})
 		.catch((error) => {
 			res
