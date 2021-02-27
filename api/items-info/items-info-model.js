17edde2c6d6973325c7856a2ffc082acbe822f69
Jessica Cooper <jessicagcooper@gmail.com>
2021-02-27 16:38:43 -0600

    Refactored items-info router/model to create separate items-color router/model.

--- api/items-info/items-info-model.js
+++ api/items-info/items-info-model.js
@@ -7,15 +7,11 @@ module.exports = {
 	findAllColors,
 	findAllMaterials,
 	findAllGarmentTitles,
-	findColorsByItemId,
 	findMaterialsByItemId,
-	findItemsByColorId,
 	findItemsByMaterialId,
 	addItemInfo,
 	addItemMaterials,
-	addItemColors,
-	removeItemMaterial,
-	removeItemColor
+	removeItemMaterial
 };
 
 function find() {
@@ -48,46 +44,12 @@ function findByItemId(id) {
 	return db('items').where({ id }).first();
 }
 
-//findColorsByItemId
-function findColorsByItemId(item_id) {
-	return db('item_colors as ic')
-		.select('ic.item_id', 'ic.color_id', 'colors.color')
-		.join('colors', 'ic.color_id', 'colors.id')
-		.where('item_id', item_id);
-}
-
 //findMaterialsByItemId
 function findMaterialsByItemId(item_id) {
 	return db('item_materials as im')
 		.select('im.item_id', 'im.material_id', 'materials.material')
 		.join('materials', 'im.material_id', 'materials.id')
 		.where('item_id', item_id);
-}
-
-//findItemsByColorId
-function findItemsByColorId(color_id) {
-	return db('item_colors as ic')
-		.join('colors', 'ic.color_id', 'colors.id')
-		.join('items', 'ic.item_id', 'items.id')
-		.select(
-			'ic.item_id',
-			'ic.color_id',
-			'colors.color',
-			'items.garment_title',
-			'items.garment_type',
-			'items.begin_year',
-			'items.end_year',
-			'items.decade',
-			'items.secondary_decade',
-			'items.culture_country',
-			'items.collection',
-			'items.collection_url',
-			'items.creator',
-			'items.source',
-			'items.item_collection_no',
-			'items.description'
-		)
-		.where('color_id', color_id);
 }
 
 //findItemsByMaterialId
@@ -136,17 +98,6 @@ async function addItemInfo(item) {
 
 	return findByItemId(id);
 }
-//post item-colors with item_id
-function addItemColors(item_id, color_fields) {
-	//color_fields should be an object containing an array of objects named fields (json format)
-	//such as { 'fields': [{'id': 2, 'color': 'red'}, {'id': 6, 'color': turquoise}] }
-	const fieldsToInsert = color_fields.map((color_field) => ({
-		item_id: item_id,
-		color_id: color_field.id //how is this data going to come from frontend?
-	}));
-	//fieldsToInsert needs to be an array of objects, via knex, postgresql will then insert each object as separate row
-	return db('item_colors').insert(fieldsToInsert).returning('*');
-}
 
 //post item-materials with item_id
 function addItemMaterials(item_id, material_fields) {
@@ -166,14 +117,6 @@ function removeItemMaterial(item_id, material_id) {
 		.where({ item_id })
 		.andWhere({ material_id })
 		.del(['item_id', 'material_id']);
-}
-
-//Delete Item_Material
-function removeItemColor(item_id, color_id) {
-	return db('item_materials')
-		.where({ item_id })
-		.andWhere({ color_id })
-		.del(['item_id', 'color_id']);
 }
 
 //PUT
