20715b422b3a140a5e8ad2853c916bc96b90ba45
Jessica Cooper <jessicagcooper@gmail.com>
2021-03-02 00:51:18 -0600

    Changed naming in middleware for accuracy.

--- api/items-info/items-info-middleware.js
+++ api/items-info/items-info-middleware.js
@@ -1,41 +1,38 @@
+/* eslint-disable prettier/prettier */
 const Items = require('./items-info-model');
 
 const checkForDuplicateItem = async (req, res, next) => {
-	const incoming_collection_url = req.body.collection_url;
-	const incoming_collection_no = req.body.item_collection_no;
+	const new_collection_url = req.body.collection_url;
+	const new_collection_no = req.body.item_collection_no;
 
-	const duplicate_url = await Items.findByCollectionUrl(
-		incoming_collection_url
-	);
-	const duplicate_collection_no = await Items.findByCollectionNo(
-		incoming_collection_no
-	);
+	const existing_url = await Items.findByCollectionUrl(new_collection_url);
+	const existing_collection_no = await Items.findByCollectionNo(new_collection_no);
 
 	if (
-		duplicate_url === undefined &&
-		duplicate_collection_no === undefined
+		existing_url === undefined &&
+		existing_collection_no === undefined
 	) {
 		next();
 	} else if (
-		duplicate_url !== undefined &&
-		duplicate_collection_no !== undefined
+		existing_url !== undefined &&
+		existing_collection_no !== undefined
 	) {
 		res.status(400).json({
-			message: `An item, id no. ${duplicate_url.id}, with the url ${duplicate_url.collection_url} and collection no. ${duplicate_collection_no.item_collection_no} already exists.`
+			message: `An item, id no. ${existing_url.id}, with the url ${existing_url.collection_url} and collection no. ${existing_collection_no.item_collection_no} already exists.`
 		});
 	} else if (
-		duplicate_url !== undefined &&
-		duplicate_collection_no === undefined
+		existing_url !== undefined &&
+		existing_collection_no === undefined
 	) {
 		res.status(400).json({
-			message: `An item with url ${duplicate_url.collection_url} exists. The collection no. may not be correct. Please review item with id no. ${duplicate_url.id} and verify the collection no. is correct.`
+			message: `An item with url ${existing_url.collection_url} exists. The collection no. may not be correct. Please review item with id no. ${existing_url.id} and verify the collection no. is correct.`
 		});
 	} else if (
-		duplicate_url === undefined &&
-		duplicate_collection_no !== undefined
+		existing_url === undefined &&
+		existing_collection_no !== undefined
 	) {
 		res.status(400).json({
-			message: `An item with url ${duplicate_collection_no.item_collection_no} exists. The collection url may not be correct. Please review item with id no. ${duplicate_collection_no.id} and verify the collection url is correct.`
+			message: `An item with url ${existing_collection_no.item_collection_no} exists. The collection url may not be correct. Please review item with id no. ${existing_collection_no.id} and verify the collection url is correct.`
 		});
 	}
 };
