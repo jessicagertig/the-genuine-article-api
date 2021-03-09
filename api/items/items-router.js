fe7e49d5c476409a9cbddf4452c9e8c4b2eb272e
Jessica Cooper <jessicagcooper@gmail.com>
2021-03-09 02:56:48 -0600

    Created route to post new item including info, colors, materials. Transactional. Modified get function.

---
+++ api/items/items-router.js
@@ -0,0 +1,95 @@
+/* eslint-disable prettier/prettier */
+const router = require('express').Router();
+// const { checkForDuplicateItem } = require('./items-info-middleware');
+const Items = require('./items-model');
+const ItemsInfo = require('../items-info/items-info-model');
+const Colors = require('../items-colors/items-colors-model');
+const Materials = require('../items-materials/items-materials-model');
+
+router.get('/', async (req, res) => {
+	try {
+		const item_info = await ItemsInfo.find();
+		let result = [];
+		if (item_info) {
+			for (let i = 0; i < item_info.length; i++) {
+				//iterate over each item found
+				let item = item_info[i];
+				//create list of colors
+				let colors = await Colors.findColorsByItemId(item_info[i].id);
+				if (colors.length > 0) {
+					colors = colors.map((color) => color.color);
+					//add list to item object
+					item['colors'] = colors;
+				} else {
+					res
+						.status(400)
+						.json({ message: 'The record is incomplete.  No colors are listed' });
+					break;
+				}
+				//create list of materials
+				let materials = await Materials.findMaterialsByItemId(item_info[i].id);
+				if (materials.length > 0) {
+					materials = materials.map((material) => material.material);
+					//add list to item object
+					item['materials'] = materials;
+				} else {
+					res
+						.status(400)
+						.json({	message: 'The record is incomplete.  No materials are listed' });
+					break;
+				}
+				result.push(item);
+			}
+			res.status(200).json(result);
+		} else {
+			res.status(400).json({
+				message: 'There are no items listed. Error on client end.'
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: 'Error on server end getting all items.',
+			error
+		});
+	}
+});
+
+//get menus, populate dropdown menus on clientside
+router.get('/menus', (req, res) => {
+	const menus = [
+		ItemsInfo.findAllColors(),
+		ItemsInfo.findAllMaterials(),
+		ItemsInfo.findAllGarmentTitles()
+	];
+	// eslint-disable-next-line prettier/prettier
+	Promise.all(menus)
+		.catch((error) => {
+			console.log('Error in retrieving menus.', error);
+		})
+		.then((menus) => {
+			const [colors, materials, garment_titles] = menus;
+			res.status(200).json({
+				colors,
+				materials,
+				garment_titles
+			});
+		});
+});
+
+//post an item
+router.post('/', async (req, res) => {
+	try {
+		const newItem = await Items.createItem(
+			req.body.item_info,
+			req.body.item_colors,
+			req.body.item_materials
+		);
+		return res.status(201).json(newItem);
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ Message: 'Error creating new item.', error });
+	}
+});
+
+module.exports = router;
