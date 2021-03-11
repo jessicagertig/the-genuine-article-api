096ea9b31f5d4950de4388c3ee38692dbd7658ad
Jessica Cooper <jessicagcooper@gmail.com>
2021-03-11 02:21:11 -0600

    Moved getColorsByItemId and getMaterialsByItemID to different model file.

--- api/items-colors/items-colors-model.js
+++ api/items-colors/items-colors-model.js
@@ -1,19 +1,10 @@
 const db = require('../../database/db-config');
 
 module.exports = {
-	findColorsByItemId,
 	findItemsByColorId,
 	addItemColors,
 	removeItemColor
 };
-
-//findColorsByItemId
-function findColorsByItemId(item_id) {
-	return db('item_colors as ic')
-		.select('ic.item_id', 'ic.color_id', 'colors.color')
-		.join('colors', 'ic.color_id', 'colors.id')
-		.where('item_id', item_id);
-}
 
 //findItemsByColorId
 function findItemsByColorId(color_id) {
