615102ec45b6e6ab0fb89ed8de5891adf2cf8c2e
Jessica Cooper <jessicagcooper@gmail.com>
2021-05-22 21:33:33 -0500

    Get all items (with colors & materials) working.

--- api/items/items-router.js
+++ api/items/items-router.js
@@ -3,45 +3,46 @@ const router = require('express').Router();
 // const { checkForDuplicateItem } = require('./items-info-middleware');
 const Items = require('./items-model');
 const ItemsInfo = require('../items-info/items-info-model');
-const Colors = require('../items-colors/items-colors-model');
-const Materials = require('../items-materials/items-materials-model');
 
 //work in progress -> attempting to filter out any items that do not include a color and a material?
 router.get('/', async (req, res) => {
   try {
     const item_info = await ItemsInfo.find();
-    let result = [];
+    console.log(item_info)
+    let result = {}
+    let itemsList = []
+    let itemsNeedingColors = []
+    let itemsNeedingMaterials = []
     if (item_info) {
       for (let i = 0; i < item_info.length; i++) {
         //iterate over each item found
         let item = item_info[i];
         //create list of colors
-        let colors = await Colors.findColorsByItemId(item_info[i].id);
+        let colors = await Items.findColorsByItemId(item.id);
         if (colors.length > 0) {
           colors = colors.map((color) => color.color);
           //add list to item object
           item['colors'] = colors;
         } else {
-          res
-            .status(400)
-            .json({ message: 'The record is incomplete.  No colors are listed' });
-          break;
+          item['colors'] = []
+          itemsNeedingColors.push(item.id)
         }
         //create list of materials
-        let materials = await Materials.findMaterialsByItemId(item_info[i].id);
+        let materials = await Items.findMaterialsByItemId(item_info[i].id);
         if (materials.length > 0) {
           materials = materials.map((material) => material.material);
           //add list to item object
           item['materials'] = materials;
         } else {
-          res
-            .status(400)
-            .json({  message: 'The record is incomplete.  No materials are listed' });
-          break;
+          item['materials'] = []
+          itemsNeedingMaterials.push(item.id)
         }
-        result.push(item);
+        itemsList.push(item);
       }
-      res.status(200).json(result);
+      result['items_list'] = itemsList
+      result['items_without_colors'] = itemsNeedingColors
+      result['items_without_materials'] = itemsNeedingMaterials
+      res.status(200).json(result)
     } else {
       res.status(400).json({
         message: 'There are no items listed. Error on client end.'
@@ -55,6 +56,52 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+//get menus, populate dropdown menus on clientside
+//function must come before get by item_id else pg will try to insert 'menus' as id
+router.get('/menus', async (req, res) => {
+  try {
+    const color_menu = await Items.findAllColors()
+    const materials_menu = await Items.findAllMaterials()
+    const garment_titles_menu = await Items.findAllGarmentTitles()
+    if (!color_menu) {
+      throw 'Error fetching the color menu.'
+    }
+    if (!materials_menu) {
+      throw 'Error fetching the materials menu.'
+    }
+    if (!garment_titles_menu) {
+      throw 'Error fetching the garment titles menu.'
+    }
+    const menus = {
+      color_menu: color_menu,
+      materials_menu: materials_menu,
+      garment_titles_menu: garment_titles_menu
+    }
+    return res
+      .status(200)
+      .json(menus)
+    // const menus = [
+      //   Items.findAllColors(),
+      //   Items.findAllMaterials(),
+      //   Items.findAllGarmentTitles()
+      // ];
+      // Promise.all(menus)
+      //   .then((menus) => {
+        //     const [colors, materials, garment_titles] = menus;
+        //     res.status(200).json({
+          //       colors,
+          //       materials,
+          //       garment_titles
+          //     });
+          //   });
+  } catch (error) {
+    return res
+    .status(500)
+    .json({ Message: 'Error fetching menus.', error });
+  }
+})
+      
 //get item by item id
 router.get('/:item_id', async (req, res) => {
   const item_id = req.params.item_id;
@@ -69,31 +116,6 @@ router.get('/:item_id', async (req, res) => {
     .json({ Message: `Error retriveing item with id ${item_id}.`, error });
   }
 })
-
-//get menus, populate dropdown menus on clientside
-//deal properly with error handling?  Are menu sizes worth using async?
-//NOT WORKING 
-router.get('/menus', (req, res) => {
-  try {
-    const menus = [
-      Items.findAllColors(),
-      Items.findAllMaterials(),
-      Items.findAllGarmentTitles()
-    ];
-    // eslint-disable-next-line prettier/prettier
-    Promise.all(menus)
-      .then((menus) => {
-        const [colors, materials, garment_titles] = menus;
-        res.status(200).json({
-          colors,
-          materials,
-          garment_titles
-        });
-      });
-  } catch (error) {
-    console.log("Error", error)
-  }
-});
 
 //post an item
 router.post('/', async (req, res) => {
