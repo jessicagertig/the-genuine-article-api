9922dd274635cfad375a5dd160430249a270b264
Jessica Cooper <jessicagcooper@gmail.com>
2023-05-14 18:34:45 -0500

    Refactor create item function to return only data that has been successfully inserted.

--- api/items/items-model.js
+++ api/items/items-model.js
@@ -1,6 +1,7 @@
 const db = require('../../database/db-config')
 const Colors = require('../items-colors/items-colors-model')
 const Materials = require('../items-materials/items-materials-model')
+const { withTransaction } = require('../utils/withTransaction')
 
 module.exports = {
   findAllGarmentTitles,
@@ -57,6 +58,7 @@ function findInfoById(id) {
 }
 
 //findItemById
+/* note: the color and material tables are separate from the items table because each item can have multiple colors and materials, and each color and material can be associated with multiple items. The item_colors and item_materials tables are join tables. Sepration also facilitates searching by color or material. */
 async function findItemById(id) {
   const info = await findInfoById(id)
   const colors = await Colors.findColorsByItemId(id)
@@ -85,52 +87,63 @@ async function getAllItems() {
   return info
 }
 
+/* 
+  Refactored createItem function to use transactions.
+  Abstracted the transaction logic to a separate file (withTransaction.js).
+  Refactored to use async/await and allow for access to returned data.
+  Driving the refactor was the prior faulty reliance on the request body to provide the item info.
+  Sources for the refactor: https://knexjs.org/guide/transactions.html and
+  https://github.com/eventuate-tram-examples/eventuate-tram-examples-nodejs-customers-and-orders/blob/8c19f208439e2dfc9c5a9221efae6cd36a299993/common-module/mysql-lib/utils.js 
+*/
 async function createItem(item_info, item_colors, item_materials) {
-  return db.transaction((trx) => {
-    return db('items')
+  return withTransaction(async (trx) => {
+    // insert item info and return the info including the new id
+    const new_item_info = await db('items')
       .insert(item_info)
       .transacting(trx)
-      .returning('id')
-      .then((res) => {
-        const item_id = res[0]
-        const colorFieldsToInsert = item_colors.map(
-          (item_color) => ({
-            item_id: item_id,
-            color_id: item_color.color_id //how is this data going to come from frontend?
-          })
-        )
-        return db('item_colors')
-          .insert(colorFieldsToInsert)
-          .transacting(trx)
-          .returning(['item_id', 'color_id'])
+      .returning('*')
+    // assign the new item id to a variable to use in the colors and materials inserts
+    const new_item_id = new_item_info[0].id
+
+    // handle the color insert
+    const colorFieldsToInsert = item_colors.map((item_color) => ({
+      item_id: new_item_id,
+      color_id: item_color.color_id
+    }))
+
+    const new_item_colors = await db('item_colors')
+      .insert(colorFieldsToInsert)
+      .transacting(trx)
+      .returning(['item_id', 'color_id'])
+    // define color_ids for return
+    const color_ids = new_item_colors.map((color) => color.color_id)
+
+    // handle the material insert
+    const materialFieldsToInsert = item_materials.map(
+      (item_material) => ({
+        item_id: new_item_id,
+        material_id: item_material.material_id
       })
-      .then((res) => {
-        const item_id = res[0].item_id
-        const color_ids = res.map((color) => color.color_id)
-        const materialFieldsToInsert = item_materials.map(
-          (item_material) => ({
-            item_id: item_id,
-            material_id: item_material.material_id //how is this data going to come from frontend?
-          })
-        )
-        return db('item_materials')
-          .insert(materialFieldsToInsert)
-          .transacting(trx)
-          .returning(item_id)
-          .then((res) => {
-            const item = {
-              item_id: res[0],
-              item_info: item_info,
-              color_ids: color_ids,
-              material_ids: materialFieldsToInsert.map(
-                (material) => material.material_id
-              )
-            }
-            return item
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
+    )
+
+    const new_item_materials = await db('item_materials')
+      .insert(materialFieldsToInsert)
+      .transacting(trx)
+      .returning(['item_id', 'material_id'])
+    // define material_ids for return
+    const material_ids = new_item_materials.map(
+      (material) => material.material_id
+    )
+
+    // define the new item object for return
+    const new_item = {
+      item_id: new_item_id,
+      item_info: new_item_info,
+      color_ids: color_ids,
+      material_ids: material_ids
+    }
+
+    return new_item
   })
 }
 
