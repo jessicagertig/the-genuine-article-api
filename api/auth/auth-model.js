87b87c62e66fc9079a022b3c0bb3bf28eb780166
Jessica Cooper <jessicagcooper@gmail.com>
2023-05-15 18:40:56 -0500

    Format auth directory files.

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -1,29 +1,29 @@
-const db = require('../../database/db-config')
+const db = require('../../database/db-config');
 
 module.exports = {
   add,
   find,
   findBy,
   findById
-}
+};
 
 function find() {
-  return db('users').select('id', 'username', 'email').orderBy('id')
+  return db('users').select('id', 'username', 'email').orderBy('id');
 }
 
 function findBy(filter) {
-  return db('users').where(filter)
+  return db('users').where(filter).first();
 }
 
 function findById(id) {
   return db('users')
     .select('id', 'username', 'email')
     .where({ id })
-    .first()
+    .first();
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user, 'id')
-
-  return findById(id)
+  const new_user_id = await db('users').insert(user, 'id');
+  const user_id = new_user_id[0].id;
+  return findById(user_id);
 }
