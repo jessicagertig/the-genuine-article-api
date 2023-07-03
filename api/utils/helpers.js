4f22a93dbad8dc2b6ecf64176be8dadbdaaf4c7c
Jessica Cooper <jessicagcooper@gmail.com>
2023-07-03 15:06:53 -0500

    Have BE calculate decades based on years.

--- api/utils/helpers.js
+++ api/utils/helpers.js
@@ -1,8 +1,4 @@
-module.exports = {
-  calculateDecades
-};
-
-const decadesDictionary = {
+const decades_dictionary = {
   0: '1800s',
   1: '1810s',
   2: '1820s',
@@ -15,17 +11,35 @@ const decadesDictionary = {
   9: '1890s'
 };
 
-const calculateDecades = (beginYear, endYear) => {
-  const getBeginDecade = beginYear.charAt(2);
-  const getEndDecade = endYear.charAt(2);
-
-  const beginDecade = decadesDictionary[getBeginDecade];
-  let endDecade;
-  if (getBeginDecade === getEndDecade) {
-    endDecade = null;
-  } else {
-    endDecade = decadesDictionary[getEndDecade];
+const calculateDecades = (begin_year, end_year) => {
+  let begin_year_string = begin_year;
+  let end_year_string = end_year;
+  if (typeof begin_year === 'number') {
+    begin_year_string = begin_year.toString();
   }
 
-  return [beginDecade, endDecade];
+  if (typeof end_year === 'number') {
+    end_year_string = end_year.toString();
+  }
+
+  const get_begin_decade = begin_year_string.charAt(2);
+  const get_end_decade =
+    end_year_string !== null ? end_year_string.charAt(2) : null;
+
+  const begin_decade = decades_dictionary[get_begin_decade];
+  let end_decade;
+  if (
+    get_begin_decade === get_end_decade ||
+    get_end_decade === null
+  ) {
+    end_decade = null;
+  } else {
+    end_decade = decades_dictionary[get_end_decade];
+  }
+
+  return [begin_decade, end_decade];
+};
+
+module.exports = {
+  calculateDecades
 };
