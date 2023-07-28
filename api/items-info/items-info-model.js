d0425eac527e858c3775bb8e09085198ba9ce17d
Jessica Cooper <jessicagcooper@gmail.com>
2023-07-28 11:10:38 -0500

    Add model & route functions.

--- api/items-info/items-info-model.js
+++ api/items-info/items-info-model.js
@@ -1,5 +1,6 @@
 const db = require('../../database/db-config');
 const { calculateDecades } = require('../utils/helpers');
+const { scrapeMET } = require('../scraper');
 
 module.exports = {
   findAllItemsInfo,
@@ -9,6 +10,7 @@ module.exports = {
   findByCollectionNo,
   findByCollectionUrl,
   addItemInfo,
+  addScrapedItemInfo,
   editItemInfo,
   deleteItemInfo
 };
@@ -64,6 +66,28 @@ function findByCollectionNo(item_collection_no) {
 //findInfoById
 function findInfoById(id) {
   return db('items').where({ id }).first();
+}
+
+// addItemScrapedInfo
+async function addScrapedItemInfo(src, url) {
+  let item_info;
+  if (src === 'MET') {
+    item_info = await scrapeMET(url);
+  }
+
+  const decadesArray = calculateDecades(
+    item_info['begin_year'],
+    item_info['end_year']
+  );
+
+  item_info['decade'] = decadesArray[0];
+  item_info['secondary_decade'] = decadesArray[1];
+
+  const new_item_info = await db('items')
+    .insert(item_info)
+    .returning('*');
+
+  return new_item_info;
 }
 
 // addItemInfo
