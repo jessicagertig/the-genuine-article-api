fcb9d9a46eb0bd1215aa8b51558da29c1be73c5f
Jessica Cooper <jessicagcooper@gmail.com>
2023-08-19 22:44:19 -0500

    Add restricted middleware to all write routes.

--- api/items-info/items-info-router.js
+++ api/items-info/items-info-router.js
@@ -3,21 +3,27 @@ const ItemsInfo = require('../items-info/items-info-model');
 const {
   checkForDuplicateUrl
 } = require('../items/items-middleware');
+const restricted = require('../auth/restricted_middleware');
 
 // post a url and have item-info scraped
 // currently options are 'MET' 'VA' or 'CAM'
-// body format: { "url": "", "src": "MET"}
-router.post('/', checkForDuplicateUrl, async (req, res) => {
-  console.log('BODY:', req.body);
-  const url = req.body.url;
-  try {
-    const newItem = await ItemsInfo.addScrapedItemInfo(url);
-    return res.status(201).json(newItem);
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ Message: 'Error creating new item.', error });
+// body format: { "url": "" }
+router.post(
+  '/',
+  restricted,
+  checkForDuplicateUrl,
+  async (req, res) => {
+    console.log('BODY:', req.body);
+    const url = req.body.url;
+    try {
+      const newItem = await ItemsInfo.addScrapedItemInfo(url);
+      return res.status(201).json(newItem);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ Message: 'Error creating new item.', error });
+    }
   }
-});
+);
 
 module.exports = router;
