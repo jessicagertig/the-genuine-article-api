9d1dcf19ba2b947b651bf909da832db594e4724d
Jessica Cooper <jessicagcooper@gmail.com>
2023-08-29 21:36:12 -0500

    Handle auth errors properly.

--- api/auth/restricted_middleware.js
+++ api/auth/restricted_middleware.js
@@ -2,46 +2,46 @@ const jwt = require('jsonwebtoken');
 const UserAuth = require('./auth-model');
 
 module.exports = (req, res, next) => {
-  let token = null;
   const auth_header = req.headers.authorization;
+  console.log('auth_header', auth_header);
 
-  // ensure correctly formatted auth header
-  if (auth_header.startsWith('Bearer ')) {
-    token = auth_header.substring(7, auth_header.length);
-  } else {
-    res
+  if (!auth_header || !auth_header.startsWith('Bearer ')) {
+    return res
       .status(401)
       .json({ message: 'Invalid authorization header.' });
   }
 
-  if (token) {
-    const secret = process.env.JWT_SECRET;
+  const token = auth_header.substring(7, auth_header.length);
 
-    jwt.verify(token, secret, function (err, decodedToken) {
-      if (err) {
-        res.status(401).json({
-          message: 'You are not authorized.',
-          err
-        });
-      } else {
-        // get user from database
-        UserAuth.findById(decodedToken.id)
-          .then((user) => {
-            if (user) {
-              req.user = user;
-              next();
-            } else {
-              res.status(401).json({ message: 'User not found!' });
-            }
-          })
-          .catch((err) => {
-            res
-              .status(500)
-              .json({ message: 'Error retrieving user!', err });
-          });
-      }
-    });
-  } else {
-    res.status(400).json({ message: 'No token provided.' });
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided.' });
   }
+
+  const secret = process.env.JWT_SECRET;
+
+  jwt.verify(token, secret, function (err, decodedToken) {
+    if (err) {
+      return res.status(401).json({
+        message: 'You are not authorized.',
+        err
+      });
+    }
+
+    UserAuth.findById(decodedToken.id)
+      .then((user) => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          return res
+            .status(404)
+            .json({ message: 'User not found!' });
+        }
+      })
+      .catch((err) => {
+        return res
+          .status(500)
+          .json({ message: 'Error retrieving user!', err });
+      });
+  });
 };
