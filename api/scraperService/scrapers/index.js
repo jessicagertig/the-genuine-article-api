e9fe193b56236353fd8b7e4384c8b361dffb902d
Jessica Gertig <jessicamgertig@gmail.com>
2024-06-19 13:33:53 -0500

    Index for all specific scrapers

---
+++ api/scraperService/scrapers/index.js
@@ -0,0 +1,21 @@
+const METScraper = require('./METScraper');
+const VAScraper = require('./VAScraper');
+const CAMScraper = require('./CAMScraper');
+const PHILAScraper = require('./PHILAScraper');
+const LACMAScraper = require('./LACMAScraper');
+const FITScraper = require('./FITScraper');
+const ROMScraper = require('./ROMScraper');
+const CWScraper = require('./CWScraper');
+const MFABScraper = require('./MFABScraper');
+
+module.exports = {
+  MET: METScraper,
+  VA: VAScraper,
+  CAM: CAMScraper,
+  PHILA: PHILAScraper,
+  LACMA: LACMAScraper,
+  FIT: FITScraper,
+  ROM: ROMScraper,
+  CW: CWScraper,
+  MFAB: MFABScraper
+};
